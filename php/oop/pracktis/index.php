9d733689bcf1464b9409bcf2c358a237dc7d5c8c
alex <yarem4uk@gmail.com>
2016-10-13 15:45:15 +0300

    home two

--- php/oop/pracktis/index.php
+++ php/oop/pracktis/index.php
@@ -2,20 +2,18 @@
 
 $arr = [2, 9, [1], [2, [4]]];
 
-
-/* $result = array_reduce($arr, function($acc, $item) { */ 
+/* $iter = function($arr, $acc) { */
+/*     $item = array_shift($arr); */
+/*     if ($item === null) */
+/*         return $acc; */
 /*     if (!is_array($item)) */
 /*         $acc[] = $item; */
-/*     return $acc; */
-/* }, []); */
+/*     else */ 
+/*         return $iter($arr, $acc); */
+/* }; */
 
-/* print_r($result); */
 
-function flatten($arr) {
-    $iter = function($item, $acc) use (&$iter) {
-        if ($item === null)
-            return $acc;
-        return $iter();
-    };
-    return $iter($arr, []);
-}
+/* print_r($iter($arr, [])); */
+$ff = [];
+$k = array_shift($ff);
+print_r($k);
