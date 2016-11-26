b4a4bd56be628f8613afbb7c735eb215b2b2ba9c
alex <yarem4uk@gmail.com>
2016-11-27 01:50:05 +0200

    add overload test'

--- php/oop/overloading/DynamicProps.php
+++ php/oop/overloading/DynamicProps.php
@@ -6,25 +6,44 @@ require_once 'DynamicPropsUndefindedProp.php';
 
 class DynamicProps
 {
-    private $arr = [];
+    private $arr;
 
-    public function __construct($arr)
+    public function __construct($arr = [])
     {
         $this->arr = $arr;
+    }
+
+    public function getarr()
+    {
+        return $this->arr;
     }
 
     public function __set($name, $value)
     {
         return $this->arr[$name] = $value;
     }
-
+    
     public function __get($name)
     {
-        
+        try {
+            if(!isset($this->arr[$name])){
+                throw new \Exception\DynamicPropsUndfindedProp();
+            }
+                return $this->arr[$name];
+        } catch (\Exception\DynamicPropsUndefinedProp $e)
+        {
+            echo $e->getMessage();
+        }
+    }
+    
+    public function __isset($name)
+    {
+        return isset($this->arr[$name]);
     }
 
-    public function getArr()
+    public function __unset($name)
     {
-        return $this->arr;
+        unset($this->arr[$name]);
     }
+
 }
--- php/oop/overloading/index.php
+++ php/oop/overloading/index.php
@@ -4,9 +4,24 @@ namespace App;
 
 require_once 'DynamicProps.php';
 
-$ob = new DynamicProps([]); 
+$ob = new DynamicProps(); 
 
-/* $ob->custom('alex', 74); */
-/* print_r($ob->arr); */
-var_dump($ob);
-print_r($ob->getArr);
+/* $ob->setCustom('root', '/'); */
+/* $ob->setArr('user', '/user'); */
+
+/* var_dump($ob->getArr); */
+/* var_dump($ob); */
+
+/* print_r($ob->getCustom()); */
+$ob->custom = 'hellow';
+/* $ob->custom = ['name'=>'url']; */
+/* var_dump($ob->custom); */
+/* echo '\n'; */
+var_dump($ob->getarr());
+echo $ob->custom;
+echo "\n";
+print_r($ob->getarr());
+echo "\n";
+unset($ob->custom);
+echo "\n";
+print_r($ob->getarr());
